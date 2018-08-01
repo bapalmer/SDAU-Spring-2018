15bbd47f739f2065310f50ce68edd5ae94060daa
bapalmer <30694114+bapalmer@users.noreply.github.com>
2018-08-01 16:34:34 +0100

    Add files via upload

---
+++ ws1_script1_navigating_R_packages.R
@@ -0,0 +1,32 @@
+####################################################################
+#                     The tidyverse and beyond 
+#                           Workshop 1
+#           Take a parachute and jump (into the tidyverse)
+#                        13th February 2018
+#                     1: Navigating R packages
+####################################################################
+# The power of R is derived from the packages that can be loaded and run
+
+# The steps below will help you load a package and navigate through 
+# some of the package contents
+
+# You need to load packages for each new session
+# 1. Type library(tidyverse) into the console below
+
+# "library" is a function that will load and attach packages
+
+# Note any messages that get displayed when the tidyverse is loaded
+
+# 2. Type "dplyr::" and scroll through the list of functions/data
+
+# 3. Access additional information by typing "?" followed by the function
+# e.g. "?dplyr::select"
+
+# You can also get more information on the sample data sets by using "?"
+# 4. Type "?mpg" into the console
+
+# R is case sensitive
+# 5. Type "r <- 5"
+# Type "r" and hit return
+
+# Type "R" and hit return
